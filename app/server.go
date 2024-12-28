c076b4954982b1de7f9800c00ee27991afe2508c
Naman Sharma <namansharma18899@gmail.com>
2024-12-28 16:32:18 +0530

    [any message]

--- app/server.go
+++ app/server.go
@@ -1,87 +1,86 @@
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
+	"io"
+	"log"
 	"net"
 	"os"
+	"time"
 )
 
-const ErrUnsupportedVersion int16 = 35
+var TAG_BUFFER = []byte{0x00}
 
-type Message struct {
-	Error         int16
-	CorrelationID int32
-}
-
-func Read(c net.Conn) (*Message, error) {
-	var tmp32, length int32
-	var tmp16 int16
-	out := Message{}
-	// Header
-	binary.Read(c, binary.BigEndian, &length)
-	binary.Read(c, binary.BigEndian, &tmp16) // API Key
-	binary.Read(c, binary.BigEndian, &tmp16) // API Version
-	binary.Read(c, binary.BigEndian, &tmp32)
-	out.CorrelationID = tmp32
-	if tmp16 < 0 || tmp16 > 4 {
-		out.Error = ErrUnsupportedVersion
-	}
-	// Body goes here
-	tmp := make([]byte, length-8)
-	c.Read(tmp)
-	return &out, nil
-}
-func Send(c net.Conn, b []byte) error {
-	binary.Write(c, binary.BigEndian, int32(len(b)))
-	binary.Write(c, binary.BigEndian, b)
-	return nil
-}
 func main() {
+	fmt.Println("Logs from your program will appear here!")
+	startServer()
+}
+func startServer() {
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
 	if err != nil {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+	defer conn.Close()
 
 	for {
-		// Accept an incoming connection
-		conn, err := l.Accept()
+		req, err := NewReqFromConn(conn)
 		if err != nil {
-			fmt.Println("Error accepting connection:", err)
-			continue
+			log.Fatal(err)
 		}
-
-		// Handle the connection in a separate goroutine
-		go handleConnection(conn)
+		fmt.Println(req)
+		res := bytes.NewBuffer([]byte{})
+		binary.Write(res, binary.BigEndian, uint32(req.CorrelationID))
+		var errorCode = uint16(0)
+		if int(req.ApiVersion) < 0 || int(req.ApiVersion) > 4 {
+			errorCode = 35
+		}
+		binary.Write(res, binary.BigEndian, uint16(errorCode))
+		binary.Write(res, binary.BigEndian, byte(2))
+		binary.Write(res, binary.BigEndian, uint16(18))
+		binary.Write(res, binary.BigEndian, uint16(3))
+		binary.Write(res, binary.BigEndian, uint16(4))
+		res.Write(TAG_BUFFER)
+		binary.Write(res, binary.BigEndian, uint32(0))
+		res.Write(TAG_BUFFER)
+		binary.Write(conn, binary.BigEndian, uint32(res.Len()))
+		io.Copy(conn, res)
 	}
 }
 
-func handleConnection(c net.Conn) {
-	m, err := Read(c)
+type Request struct {
+	ApiKey        uint16
+	ApiVersion    uint16
+	CorrelationID uint32
+	ClientId      string
+	TaggedFields  string
+}
 
+func NewReqFromConn(conn net.Conn) (Request, error) {
+	var size uint32
+	err := binary.Read(conn, binary.BigEndian, &size)
 	if err != nil {
-		fmt.Println("Error reading message: ", err.Error())
-		os.Exit(1)
+		return Request{}, err
 	}
-	if m.Error != 0 {
-		out := make([]byte, 6)
-		binary.BigEndian.PutUint32(out, uint32(m.CorrelationID))
-		binary.BigEndian.PutUint16(out[4:], uint16(m.Error))
-		Send(c, out)
-		os.Exit(1)
-	}
-	out := make([]byte, 19)
-	binary.BigEndian.PutUint32(out, uint32(m.CorrelationID))
-	fmt.Println("OUT -> ", out)
-	binary.BigEndian.PutUint16(out[4:], 0)  // Error code
-	out[6] = 2                              // Number of API keys
-	binary.BigEndian.PutUint16(out[7:], 18) // API Key 1 - API_VERSIONS
-	binary.BigEndian.PutUint16(out[9:], 3)  //             min version
-	binary.BigEndian.PutUint16(out[11:], 4) //             max version
-	out[13] = 0                             // _tagged_fields
-	binary.BigEndian.PutUint32(out[14:], 0) // throttle time
-	out[18] = 0
-
-	Send(c, out)
+	var req = Request{}
+	binary.Read(conn, binary.BigEndian, &req.ApiKey)
+	binary.Read(conn, binary.BigEndian, &req.ApiVersion)
+	binary.Read(conn, binary.BigEndian, &req.CorrelationID)
+	conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+	conn.Read(make([]byte, 1024))
+	return req, nil
+}
+func (r *Request) Reader() (uint32, io.Reader) {
+	var buff bytes.Buffer
+	binary.Write(&buff, binary.BigEndian, r.ApiKey)
+	binary.Write(&buff, binary.BigEndian, r.ApiVersion)
+	binary.Write(&buff, binary.BigEndian, r.CorrelationID)
+	return uint32(buff.Len()), &buff
 }
